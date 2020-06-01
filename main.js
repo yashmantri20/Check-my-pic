13748ab494ec4e632b3ddb5bb475473c7e26d64d
yash mantri <mantriyash2025@gmail.com>
2020-06-01 16:29:54 +0530

    stage2

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
 let countnum;
-const check = parseInt(localStorage.getItem('counter'))
-
-const countFunc = () => {
-    if(check)
+let values = ['count1','count']
+const countFunc = (id,show) => {
+    if(localStorage.getItem(id))
     {
-        countnum = parseInt(localStorage.getItem('counter'));
+        countnum = parseInt(localStorage.getItem(id));
+        console.log(countnum)
         countnum += 1
-        localStorage.setItem('counter',countnum)
+        localStorage.setItem(id,countnum)
     }
     else{
         countnum = 1
-        localStorage.setItem('counter',countnum)
+        localStorage.setItem(id,countnum)
     }
     
-    const count = document.getElementById('count')
+    const count = document.getElementById(show)
     count.innerText = `Total Count ${countnum}`
 }
 
-document.getElementById('listen').addEventListener('click', countFunc)
+document.getElementById('listen').addEventListener('click', function(){
+    countFunc('counter','count')
+})
+
+document.getElementById('listen1').addEventListener('click', function(){
+    countFunc('counter1','count1')
+})
 
 window.onload = 
     () => {
-        countnum = parseInt(localStorage.getItem('counter'));
-        const count = document.getElementById('count')
-        count.innerText = `Total Count ${countnum}`
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+        while ( i-- ) 
+        {
+            countnum = parseInt(localStorage.getItem(keys[i]));
+            const count = document.getElementById(values[i])
+            count.innerText = `Total Count ${countnum}`
+        }
     }
